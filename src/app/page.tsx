63ece5418e6392ca149eb4c3b46819ce4f31908a
yoko345 <spblackhole89@gmail.com>
2025-08-30 15:24:41 +0900

    画像の最適化のためのNext.jsのコンポーネントの使用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -11,10 +12,12 @@ export default function Home() {
                     <h2 className={styles.description}>会社名：{compnayName}</h2>
                     <p className={styles.description}>私達は市場をリードしているグローバルテックカンパニーです！</p>
                 </div>
-                <img
+                <Image
                     className={styles.bgimg}
                     src="/img-mv.jpg"
                     alt=""
+                    width={4000}
+                    height={1200}
                 />
             </section>
         </>
