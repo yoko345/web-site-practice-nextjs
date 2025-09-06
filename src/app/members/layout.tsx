96ec839d6bfc35ffa3760a48e299a5f48cd85f9e
yoko345 <spblackhole89@gmail.com>
2025-09-06 15:17:20 +0900

    Heroコンポーネント作成により、ページを通して共通の見た目にするための学習

--- src/app/members/layout.tsx
+++ src/app/members/layout.tsx
@@ -1,9 +1,18 @@
-import Sheet from "../_components/Sheet";
+import Hero from "@/app/_components/Hero";
+import Sheet from "@/app/_components/Sheet";
 
 type Props = {
     children: React.ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-    return <Sheet>{children}</Sheet>;
+    return (
+        <>
+            <Hero
+                title="Members"
+                sub="メンバー"
+            />
+            <Sheet>{children}</Sheet>
+        </>
+    );
 }
