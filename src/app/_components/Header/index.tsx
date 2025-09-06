d86cb546116da9b04def2b66035708b9d3deb1a7
yoko345 <spblackhole89@gmail.com>
2025-09-07 07:48:56 +0900

    ハンバーガーメニューの追加の学習、classnamesパッケージのインストール

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import Link from "next/link";
+import Menu from "../Menu";
 
 export default function Header() {
     return (
@@ -18,19 +19,7 @@ export default function Header() {
                     priority
                 />
             </Link>
-            <nav className={styles.nav}>
-                <ul className={styles.items}>
-                    <li>
-                        <Link href="/news">ニュース</Link>
-                    </li>
-                    <li>
-                        <Link href="/members">メンバー</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">お問い合わせ</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Menu />
         </header>
     );
 }
