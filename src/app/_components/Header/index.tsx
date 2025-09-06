e8fa60f23144686e9078075c2a45bdbcb9f7a23b
yoko345 <spblackhole89@gmail.com>
2025-09-06 13:15:20 +0900

    ヘッダーとフッターのaタグをLinkタグに変更

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -18,10 +18,16 @@ export default function Header() {
                     priority
                 />
             </Link>
-            <nav>
+            <nav className={styles.nav}>
                 <ul className={styles.items}>
                     <li>
+                        <Link href="/news">ニュース</Link>
+                    </li>
+                    <li>
                         <Link href="/members">メンバー</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">お問い合わせ</Link>
                     </li>
                 </ul>
             </nav>
