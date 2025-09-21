1e44918387a23f75286a139818e30ccc82177625
yoko345 <spblackhole89@gmail.com>
2025-09-21 14:22:37 +0900

    Lighhouse測定時に出た画像の読み込みに関するパフォーマンス向上の仕方の学習

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,8 @@ export default async function Home() {
                     alt=""
                     width={4000}
                     height={1200}
+                    priority
+                    sizes="(max-width: 640px) 100vw,50vw"
                 />
             </section>
             <section className={styles.news}>
